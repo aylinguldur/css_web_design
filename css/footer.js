87da95ecb91c16ed0a49431468f502560a847a55
aylinguldur <aylin.guldur@gmail.com>
2022-06-03 12:53:22 +0300

    web design

---
+++ css/footer.js
@@ -0,0 +1,33 @@
+document.write('\
+<footer class="footer">\
+\<div class="column" style="text-align:left; padding:10px; display:flex">\
+\<div>\
+    \<img class="amblemFooter" src="src/amblem.png">\
+\</div>\
+\<div style="margin-left:20px;font-size:17px">\
+<p>illy Kahve</p> \
+\<p>Levazım Mahallesi, illy Cafe, Beşiktaş/İstanbul</p>\
+\<p>(0216) 123 26 55</p>\
+\<p>info@barista.com.tr</p>\
+\</div>\
+\</div>\
+\<div class="column" style="text-align:center;font-size:17px">\
+\<div style="margin-top:20px;" >\
+\<a style="color:white;" href="https://www.instagram.com/illyinturkey/">Instagram</a>\
+\</div>\
+\<div style="margin-top:20px;">\
+\<a style="color:white;" href="https://www.facebook.com/illy/">Facebook</a>\
+\</div>\
+\</div>\
+\ <div class="column"style="text-align:center;font-size:17px;margin-top:10px;">\
+\ <a href="admin.html" target="_blank" style="color:white;">\
+\   Admin Giriş \
+\ </a>\
+\<div style="margin-top:25px;">\
+\<p> Web Sitesi Tasarıcısı :</p>\
+\<p>Aylinnur Güldür</p>\
+\<p>aylin.guldur@gmail.com</p>\
+\</div>\
+</div> \
+</footer>\
+')
---
+++ css/header.js
@@ -0,0 +1,31 @@
+document.write('\
+    \<div class="header"> \
+    \<a href="home.html">\
+    \<img src="src/amblem.png" class="amblem">\
+    \</a>\
+ \      <i>illy</i>\
+   \</div>\
+  \  <nav role="navigation" class="primary-navigation">\
+    \    <ul>\
+    \       <li><a href="home.html">Anasayfa</a></li>\
+            <li><a href="misyon_vizyon.html">Misyon-Vizyon</a></li>\
+            <li><a href="#">Ürünlerimiz &dtrif;</a>\
+                <ul class="dropdown">\
+                    <li><a href="kahveCesitleri.html">Kahve Çeşitleri</a></li>\
+                    <li><a href="kahveEkipman.html">Kahve Ekipmanları</a></li>\
+                    <li><a href="kahveBardak.html">Kahve Fincanlarımız</a></li>\
+                </ul>\
+            </li>\
+            <li><a href="#">Birimlerimiz &dtrif;</a>\
+                <ul class="dropdown"> \
+                    <li><a href="#">Müşteri Hizmetleri</a></li>\
+                    <li><a href="#">Araştırma ve Geliştirme</a></li>\
+                    <li><a href="#">Satın Alma</a></li>\
+                </ul>\
+            </li>\
+            <li><a href="hakkımda.html">Hakkımızda</a></li>\
+            <li><a href="iletisim.html">İletişim</a></li>\
+        </ul>\
+    </nav>\
+ \
+')
